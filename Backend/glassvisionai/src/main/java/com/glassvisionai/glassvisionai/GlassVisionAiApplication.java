69db5754c4139929960acd91c693c44893dce403
Aditya Kumar <adityakumar0718@gmail.com>
2025-02-09 17:15:26 +0530

    backend structure

---
+++ Backend/glassvisionai/src/main/java/com/glassvisionai/glassvisionai/GlassVisionAiApplication.java
@@ -0,0 +1,13 @@
+package com.glassvisionai.glassvisionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassVisionAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassVisionAiApplication.class, args);
+	}
+
+}
---
+++ Backend/glassvisionai/src/test/java/com/glassvisionai/glassvisionai/GlassVisionAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glassvisionai.glassvisionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassVisionAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
